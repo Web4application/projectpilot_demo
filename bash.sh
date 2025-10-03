afa6900f16ca355fc71e332011ccdc23c7e5b8ce
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-03 02:26:11 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,10 @@
 flutter create projectpilot_demo
 cd projectpilot_demo
+chmod +x create_full_project.sh
+./create_full_project.sh
+cd projectpilot_full/backend
+npm install
+node server.js
+cd projectpilot_full/frontend
+flutter pub get
+flutter run -d chrome
