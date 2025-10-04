7a5676d5e988dec5f6d01d9a9bd4c9a2dac2d4b8
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-04 04:32:41 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app" class="flex flex-col h-screen">
+    <HeaderBar />
+
+    <div class="flex flex-1 overflow-hidden">
+      <SidebarMenu @selectModule="setActiveModule" />
+      <main class="flex-1 p-6 overflow-auto bg-gray-100">
+        <component :is="activeModuleComponent" />
+      </main>
+    </div>
+
+    <FooterBar />
+  </div>
+</template>
