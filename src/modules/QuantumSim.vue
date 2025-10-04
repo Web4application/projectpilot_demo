36613e977f9662b669344dd028b0b19e411dec48
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-04 04:28:00 +0100

    Update QuantumSim.vue

--- src/modules/QuantumSim.vue
+++ src/modules/QuantumSim.vue
@@ -1,1 +1,10 @@
+<template>
+  <div>
+    <h2>Quantum Simulation</h2>
+    <p>Quantum optimization dashboard and results.</p>
+  </div>
+</template>
 
+<script>
+export default { name: 'QuantumSim' };
+</script>
