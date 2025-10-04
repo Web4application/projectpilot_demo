61ab19a7fdfabd6064ae5b1e0b55a3585efbfc4e
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-04 04:25:34 +0100

    Create FooterBar.vue

---
+++ components/FooterBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer-bar">
+    <p>© 2025 ProjectPilot. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default { name: 'FooterBar' };
+</script>
+
+<style scoped>
+.footer-bar {
+  background: #1f2937;
+  color: #fff;
+  padding: 10px;
+  text-align: center;
+}
+</style>
