e24344c024ecb165f1ca0214d52cab8ebeabd14d
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-04 04:23:32 +0100

    Create HeaderBar.vue

---
+++ components/HeaderBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header-bar">
+    <h1>ProjectPilot Dashboard</h1>
+  </header>
+</template>
+
+<script>
+export default { name: 'HeaderBar' };
+</script>
+
+<style scoped>
+.header-bar {
+  background: #1f2937;
+  color: #fff;
+  padding: 15px;
+  font-size: 1.2em;
+}
+</style>
