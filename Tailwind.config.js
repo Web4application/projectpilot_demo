3eb06bfb791bcfb5542d053d63e0ca5a6ea8d3ba
Web4 <167559384+Web4application@users.noreply.github.com>
2025-10-07 12:31:24 +0100

    Create Tailwind.config.js

---
+++ Tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#3DF5FF',
+        accent: '#6D28D9'
+      },
+      animation: {
+        float: "float 6s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-15px)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
